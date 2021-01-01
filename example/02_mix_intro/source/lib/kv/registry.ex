2e14ec4b4903792f51b1fae6c5d388dfc46c35aa
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-01 15:15:38 +0100

    more files

---
+++ example/02_mix_intro/source/lib/kv/registry.ex
@@ -0,0 +1,44 @@
+defmodule KV.Registry do
+  use GenServer
+  # client api
+  @doc """
+  start registry
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, ok = opts)
+  end
+
+  @doc """
+  lookup bucket pit for name stored in server
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  ensure bucket exists with given name in server
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  @impl true
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  @impl true
+  def handle_call({:lookup, name}, _from, names) do
+    {:reply, MAp.fetch(names, name), names}
+  end
+
+  @impl true
+  def handle_cast({:create, name}, names) do
+    if Map.has_key?(names, name) do
+      {:noreply, names}
+    else
+      {:ok, bucket} = KV.Bucket.start_link([])
+      {:noreply, Map.put(names, name, bucket)}
+    end
+  end
+end
---
+++ example/02_mix_intro/source/test/kv/registry_test.exs
@@ -0,0 +1,16 @@
+defmodule KV.RegistryTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    registry = start_supervised!(KV.Registry)
+    %{:registry => registry}
+  end
+
+  test "spawn buckets", %{:registry => regitry} do
+    assert(:error == KV.Registry.lookup(registry, "shopping"))
+    KV.Registry.create(registry, "shopping")
+    assert({:ok, bucket} == KV.Registry.lookup(registry, "shopping"))
+    KV.Bucket.put(bucket, "milk", 1)
+    assert(1 == KV.Bucket.get(bucket, "milk"))
+  end
+end

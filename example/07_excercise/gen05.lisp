dc2ff0ee1cae43b274bdc69d5b21dd591d621011
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-21 23:14:58 +0100

    map example finished

--- example/07_excercise/gen05.lisp
+++ example/07_excercise/gen05.lisp
@@ -11,13 +11,57 @@
    (defmodule MapTest
      "use ExUnit.Case"
      (def sample ()
-       (dict :foo (string "bar")
+       (map :foo (string "bar")
 	     :baz (string "quz")))
 
      ,@(loop for (e f) in `((Map.get
 			     (do0
 			      (assert (== (Map.get (sample) ":foo")
-					  (string "bar"))))))
+					  (string "bar")))
+			      (assert (== (Map.get (sample) ":non_existent")
+					  "nil"))))
+			    ("[]"
+			     (do0
+			      (assert (== (string "bar")
+					  (aref (sample) ":foo")))
+			      (assert (== "nil"
+					  (aref (sample) ":non_existent")))))
+			    ("."
+			     (do0
+			      (assert (== (string "bar")
+					  (dot (sample) foo)))
+			      (assert_raise KeyError (lambda ()
+						       (dot (sample)
+							    non_existent)))))
+			    ("Map.fetch"
+			     (do0
+			      (setf (tuple ":ok" val)
+				    (Map.fetch (sample) ":foo"))
+			      (assert (== (string "bar")
+					  val))
+			      (setf ":error"
+				    (Map.fetch (sample) ":non_existent"))))
+			    ("Map.put"
+			     (do0
+			      (assert (== (Map.put (sample) ":foo" (string "bob"))
+					  (map :foo (string "bob")
+					       :baz (string "quz"))))
+			      (assert (== (Map.put (sample) ":far" (string "bar"))
+					  (map :foo (string "bob")
+					       :baz (string "quz")
+					       :far (string "bar"))))))
+			    ("update map with pattern matching syntax"
+			     (do0
+			      (assert (== (map :foo (string "bob")
+					       :baz (string "quz"))
+					  "%{ sample() | foo: 'bob' }"))
+			      (assert_raise KeyError (lambda () ;; new key should fail
+						       "%{ sample() | far: 'bob' }"))))
+			    (Map.values
+			     (do0
+			      (assert (== (list (string "bar")
+						(string "quz"))
+					  (Enum.sort (Map.values (sample))))))))
 	     collect
 	     `(space test (string ,e)
 		     (progn
--- example/07_excercise/source/05_map.exs
+++ example/07_excercise/source/05_map.exs
@@ -4,10 +4,41 @@ defmodule MapTest do
   use ExUnit.Case
 
   def sample() do
-    dict(foo: string(bar), baz: string(quz))
+    %{:foo => "bar", :baz => "quz"}
   end
 
   test "Map.get" do
     assert(Map.get(sample(), :foo) == "bar")
+    assert(Map.get(sample(), :non_existent) == nil)
+  end
+
+  test "[]" do
+    assert("bar" == sample()[:foo])
+    assert(nil == sample()[:non_existent])
+  end
+
+  test "." do
+    assert("bar" == sample().foo)
+    assert_raise(KeyError, fn -> sample().non_existent end)
+  end
+
+  test "Map.fetch" do
+    {:ok, val} = Map.fetch(sample(), :foo)
+    assert("bar" == val)
+    :error = Map.fetch(sample(), :non_existent)
+  end
+
+  test "Map.put" do
+    assert(Map.put(sample(), :foo, "bob") == %{:foo => "bob", :baz => "quz"})
+    assert(Map.put(sample(), :far, "bar") == %{:foo => "bob", :baz => "quz", :far => "bar"})
+  end
+
+  test "update map with pattern matching syntax" do
+    assert(%{:foo => "bob", :baz => "quz"} == %{sample() | foo: 'bob'})
+    assert_raise(KeyError, fn -> %{sample() | far: 'bob'} end)
+  end
+
+  test "Map.values" do
+    assert(["bar", "quz"] == Enum.sort(Map.values(sample())))
   end
 end

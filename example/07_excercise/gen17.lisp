5272f19a5918dfbd25a3ad792ac958355977f6e4
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-23 11:02:52 +0100

    philosopher code isn't workin

--- example/07_excercise/gen17.lisp
+++ example/07_excercise/gen17.lisp
@@ -13,51 +13,54 @@
 	 ate 0
 	 thought 0)
      )
-   (def simulate ()
-     (setf forks (list ,@(loop for i below 5 collect
-					     (format nil ":fork~a" i))))
-     (setf table (spawn_link Table
-			     ":manage_resource"
-			     (list forks)))
-     ,@(loop for e in `(Aris
-			Kant
-			Spin
-			Marx
-			Russ)
-	     collect
-	     `(spawn Dine ":dine"
-		     (list (struct Philosopher
-				   name (string ,e))
-			   table)))
-     (receive
+
+   (defmodule Table
+       (def simulate ()
+      (setf forks (list ,@(loop for i below 5 collect
+					      (format nil ":fork~a" i))))
+      (setf table (spawn_link Table
+			      ":manage_resources"
+			      (list forks)))
+      ,@(loop for e in `(Aris
+			 Kant
+			 Spin
+			 Marx
+			 Russ)
+	      collect
+	      `(spawn Dine ":dine"
+		      (list (struct Philosopher
+				    name (string ,e))
+			    table)))
+      (receive
 	 
-      (_ ":ok")))
-   (def manage_resources (forks &optional (waiting (list)))
-     (when (< 0 (length waiting))
-       (setf names (for ((tuple _ phil)
-			 waiting)
-			phil.name))
-       
-       ,(lprint `((length waiting)
-		  names))
-       (when (<= 2 (length forks))
-	 (setf (cons (tuple pid _)
-		     waiting)
-	       waiting
-	       (cons "fork1, fork2"
-		     forks)
-	       forks)
-	 (send pid (tuple :eat (list fork1 fork2))))
-       (receive
-	
-	( (tuple :sit_down pid phil)
-	  (manage_resources forks (cons (tuple pid phil)
-					waiting)))
-	( (tuple :give_up_seat
-		 free_forks _)
-	  (setf forks (++ free_forks forks))
-	  ,(lprint `((length forks)))
-	  (manage_resources forks waiting)))))
+       (_ ":ok")))
+     (def manage_resources (forks &optional (waiting (list)))
+       (when (< 0 (length waiting))
+	 (setf names (for ((tuple _ phil)
+			   waiting)
+			  phil.name))
+	 
+	 ,(lprint `((length waiting)
+		    names))
+	 (when (<= 2 (length forks))
+	   (setf (cons (tuple pid _)
+		       waiting)
+		 waiting
+		 (cons "fork1, fork2"
+		       forks)
+		 forks)
+	   (send pid (tuple :eat (list fork1 fork2))))
+	 (receive
+	  
+	  ( (tuple :sit_down pid phil)
+	    (manage_resources forks (cons (tuple pid phil)
+					  waiting)))
+	  ( (tuple :give_up_seat
+		   free_forks _)
+	    (setf forks (++ free_forks forks))
+	    ,(lprint `((length forks)))
+	    (manage_resources forks waiting))))))
+
    (defmodule Dine
        (def dine (phil table)
 	 (send table (tuple :sit_down self phil))
@@ -68,10 +71,11 @@
 	 (dine phil table))
      (def eat (phil forks table)
        ;; i don't like this map update syntax
-       (setf phil "%{phil | ate: phil.ate + 1}")
+       ;; (setf phil "%{phil | ate: phil.ate + 1}")
+       (setf phil (Map.update phil ":ate" 0 (lambda (x) (+ x 1))))
        ,(lprint `(phil.name (string "eating") phil.ate))
-       (":timer.sleep" (":random.unirfom 1_000"))
-       ,(lprint `(phil.nam (string "done eating") e))
+       (":timer.sleep" (":random.uniform"  1_000))
+       ,(lprint `(phil.nam (string "done eating")))
        (send table (tuple :give_up_seat forks phil))
        phil)
      (def think (phil _)
@@ -79,7 +83,7 @@
        (":timer.sleep" (":random.uniform" 1000))
        (setf phil "%{phil | thought: phil.thought + 1}")))
    (":random.seed" ":erlang.now")
-   (Table.simulate)
+   "Table.simulate"
    ))
 
 
--- example/07_excercise/source/17_dining_philosophers.exs
+++ example/07_excercise/source/17_dining_philosophers.exs
@@ -1,63 +1,93 @@
 defmodule Philosopher do
-defstruct([name: nil, ate: 0, thought: 0])
+  defstruct(name: nil, ate: 0, thought: 0)
 end
-def simulate() do
+
+defmodule Table do
+  def simulate() do
     forks = [:fork0, :fork1, :fork2, :fork3, :fork4]
-    table = spawn_link(Table, :manage_resource, [forks])
+    table = spawn_link(Table, :manage_resources, [forks])
     spawn(Dine, :dine, [%Philosopher{name: "Aris"}, table])
     spawn(Dine, :dine, [%Philosopher{name: "Kant"}, table])
     spawn(Dine, :dine, [%Philosopher{name: "Spin"}, table])
     spawn(Dine, :dine, [%Philosopher{name: "Marx"}, table])
     spawn(Dine, :dine, [%Philosopher{name: "Russ"}, table])
+
     receive do
-_ -> :ok
+      _ -> :ok
+    end
+  end
 
-end
-end
-def manage_resources(forks, waiting \\ []) do
-    if ( ((0)<(length(waiting))) ) do
-names = for {_,phil} <- waiting do
-        phil.name
-end
-    IO.puts("#{__ENV__.file}:#{__ENV__.line} length(waiting)=#{length(waiting)} names=#{names}")
-    if ( ((2)<=(length(forks))) ) do
-[{pid,_} | waiting] = waiting
-    [fork1, fork2 | forks] = forks
-    send(pid, {:eat,[fork1, fork2]})
-end
-    receive do
-{:sit_down,pid,phil} -> manage_resources(forks, [{pid,phil} | waiting])
+  def manage_resources(forks, waiting \\ []) do
+    if 0 < length(waiting) do
+      names =
+        for {_, phil} <- waiting do
+          phil.name
+        end
 
-{:give_up_seat,free_forks,_} -> forks = ((free_forks)++(forks))
-    IO.puts("#{__ENV__.file}:#{__ENV__.line} length(forks)=#{length(forks)}")
-    manage_resources(forks, waiting)
+      IO.puts("#{__ENV__.file}:#{__ENV__.line} length(waiting)=#{length(waiting)} names=#{names}")
 
+      if 2 <= length(forks) do
+        [{pid, _} | waiting] = waiting
+        [fork1, fork2 | forks] = forks
+        send(pid, {:eat, [fork1, fork2]})
+      end
+
+      receive do
+        {:sit_down, pid, phil} ->
+          manage_resources(forks, [{pid, phil} | waiting])
+
+        {:give_up_seat, free_forks, _} ->
+          forks = free_forks ++ forks
+          IO.puts("#{__ENV__.file}:#{__ENV__.line} length(forks)=#{length(forks)}")
+          manage_resources(forks, waiting)
+      end
+    end
+  end
 end
-end
-end
+
 defmodule Dine do
-def dine(phil, table) do
-    send(table, {:sit_down,self,phil})
-    receive do
-{:eat,forks} -> phil = eat(phil, forks, table)
-    phil = think(phil, table)
+  def dine(phil, table) do
+    send(table, {:sit_down, self, phil})
 
-end
+    receive do
+      {:eat, forks} ->
+        phil = eat(phil, forks, table)
+        phil = think(phil, table)
+    end
+
     dine(phil, table)
-end
-def eat(phil, forks, table) do
-    phil = %{phil | ate: phil.ate + 1}
-    IO.puts("#{__ENV__.file}:#{__ENV__.line} phil.name=#{phil.name} \"eating\"=#{"eating"} phil.ate=#{phil.ate}")
-    :timer.sleep(:random.unirfom 1_000())
-    IO.puts("#{__ENV__.file}:#{__ENV__.line} phil.nam=#{phil.nam} \"done eating\"=#{"done eating"} e=#{e}")
-    send(table, {:give_up_seat,forks,phil})
+  end
+
+  def eat(phil, forks, table) do
+    phil = Map.update(phil, :ate, 0, fn x -> x + 1 end)
+
+    IO.puts(
+      "#{__ENV__.file}:#{__ENV__.line} phil.name=#{phil.name} \"eating\"=#{"eating"} phil.ate=#{
+        phil.ate
+      }"
+    )
+
+    :timer.sleep(:random.uniform(1_000))
+
+    IO.puts(
+      "#{__ENV__.file}:#{__ENV__.line} phil.nam=#{phil.nam} \"done eating\"=#{"done eating"}"
+    )
+
+    send(table, {:give_up_seat, forks, phil})
     phil
-end
-def think(phil, _) do
-    IO.puts("#{__ENV__.file}:#{__ENV__.line} phil.name=#{phil.name} \"thinking\"=#{"thinking"} phil.thought=#{phil.thought}")
+  end
+
+  def think(phil, _) do
+    IO.puts(
+      "#{__ENV__.file}:#{__ENV__.line} phil.name=#{phil.name} \"thinking\"=#{"thinking"} phil.thought=#{
+        phil.thought
+      }"
+    )
+
     :timer.sleep(:random.uniform(1000))
     phil = %{phil | thought: phil.thought + 1}
+  end
 end
-end
-:random.seed(:erlang.now)
+
+:random.seed(:erlang.now())
 Table.simulate()

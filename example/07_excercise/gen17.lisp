18b8a4e205552c8916b3f25339eda139d556c928
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-23 07:49:15 +0100

    more code

--- example/07_excercise/gen17.lisp
+++ example/07_excercise/gen17.lisp
@@ -7,7 +7,35 @@
 (write-source
  (format nil "~a/17_dining_philosophers.exs" *path*)
  `(do0
-   
+   (defmodule Philosopher
+       (defstruct
+	   name "nil"
+	 ate 0
+	 thought 0))
+   (def simulate ()
+     (setf forks (list ,@(loop for i below 5 collect
+					     (format nil ":fork~a" i))))
+     (setf table (spawn_link Table
+			     ":manage_resource"
+			     (list forks)))
+     ,@(loop for e in `(Aris
+			Kant
+			Spin
+			Marx
+			Russ)
+	     collect
+	     `(spawn Dine ":dine"
+		     (list (struct Philosopher
+				   name (string ,e))
+			   table)))
+     (receive
+      ()
+      (-> _ ":ok")))
+   (def manage_resources (forks &optional (waiting (list)))
+     (when (< 0 (length waiting))
+       (setf names (for ((tuple _ phil)
+			 waiting)
+			phil.name))))
    ))   
 
 
--- example/07_excercise/source/17_dining_philosophers.exs
+++ example/07_excercise/source/17_dining_philosophers.exs
@@ -1,1 +1,26 @@
+defmodule Philosopher do
+  defstruct(name: nil, ate: 0, thought: 0)
+end
 
+def simulate() do
+  forks = [:fork0, :fork1, :fork2, :fork3, :fork4]
+  table = spawn_link(Table, :manage_resource, [forks])
+  spawn(Dine, :dine, [%Philosopher{name: "Aris"}, table])
+  spawn(Dine, :dine, [%Philosopher{name: "Kant"}, table])
+  spawn(Dine, :dine, [%Philosopher{name: "Spin"}, table])
+  spawn(Dine, :dine, [%Philosopher{name: "Marx"}, table])
+  spawn(Dine, :dine, [%Philosopher{name: "Russ"}, table])
+
+  receive do
+    _ -> :ok
+  end
+end
+
+def manage_resources(forks, waiting \\ []) do
+  if 0 < length(waiting) do
+    names =
+      for {_, phil} <- waiting do
+        phil.name
+      end
+  end
+end

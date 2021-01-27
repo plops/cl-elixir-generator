12738ff9797930a4c215cc75d9a098acb581e772
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-27 22:30:09 +0100

    another file

---
+++ example/08_minimal_live/source/lib/q_web/channels/user_socket.ex
@@ -0,0 +1,12 @@
+defmodule QWeb.UserSocket do
+  use Phoenix.Socket
+  @impl true
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def id(_socket) do
+    nil
+  end
+end

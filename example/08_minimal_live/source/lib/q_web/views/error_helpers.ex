9bbace53d4c224c16164d46471dfd55db9fa8827
Martin Kielhorn <kielhorn.martin@gmail.com>
2021-01-28 19:19:49 +0100

    another file

---
+++ example/08_minimal_live/source/lib/q_web/views/error_helpers.ex
@@ -0,0 +1,20 @@
+defmodule QWeb.ErrorHelpers do
+  use Phoenix.HTML
+
+  def error_tag(form, field) do
+    Enum.map(Keyword.get_values(form.errors, field), fn error ->
+      content_tag(:span, translate_error(error),
+        class: "invalid-feedback",
+        phx_feedback_for: input_id(form, field)
+      )
+    end)
+  end
+
+  def translate_error({msg, opts}) do
+    if count = opts[:count] do
+      Gettext.dngettext(QWeb.Gettext, "errors", msg, msg, count, opts)
+    else
+      Gettext.dngettext(QWeb.Gettext, "errors", msg, opts)
+    end
+  end
+end
